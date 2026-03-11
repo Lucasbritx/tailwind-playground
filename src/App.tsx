4dfcac7019f6f580c9ebb5d2ed403df6eeadebd9
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-03-10 22:24:23 -0300

    columns example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import AspectRatio from "./examples/aspectRatio";
+import Columns from "./examples/columns";
 
 function App() {
   return (
     <>
-      <div>
+      <div className="space-y-10">
         <AspectRatio />
+        <Columns />
       </div>
     </>
   );
