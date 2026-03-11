4abef69898901e0272b9233126e616960edb97dc
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-03-10 22:33:20 -0300

    feat: columns sm breakpoint for mobile

--- src/examples/columns.tsx
+++ src/examples/columns.tsx
@@ -7,7 +7,7 @@ function Columns() {
   return (
     <>
       <h2>Columns examples</h2>
-      <div className="columns-3xs gap-8">
+      <div className="columns-2 gap-4 sm:columns-3 sm:gap-8">
         <img className="aspect-3/2" src={MOUNTAINS_IMAGE_URL} />
         <img className="aspect-square" src={MOUNTAINS_IMAGE_URL} />
         <img className="aspect-square" src={MOUNTAINS_IMAGE_URL} />
