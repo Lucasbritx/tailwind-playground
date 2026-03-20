75531a99025d337ac1dbd85a055685cc24024a99
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-03-20 00:00:44 -0300

    WIP visibility

--- src/examples/visibility.tsx
+++ src/examples/visibility.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-
 function Visibility() {
   return (
     <>
       <h2>Visibility examples</h2>
 
-      <div class="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4">
         <div>01</div>
-        <div class="invisible ...">02</div>
+        <div className="invisible">02</div>
         <div>03</div>
       </div>
     </>
